2821cd504c775f9afdd69afa15b627fa26da918e
SarthakKamboj <sarthakkamboj22@gmail.com>
2024-02-19 22:06:01 -0600

    added loading for meshes, accessors, buffers, buffer views, etc

--- src/model_loading/gltf/gltf.cpp
+++ src/model_loading/gltf/gltf.cpp
@@ -12,12 +12,51 @@ char* data = NULL;
 int offset = -1;
 int data_len = -1;
 
+static int active_scene = -1;
 static std::vector<gltf_scene_t> gltf_scenes; 
 static std::vector<gltf_node_t> gltf_nodes; 
+static std::vector<gltf_mesh_t> gltf_meshes;
+static std::vector<gltf_buffer_t> gltf_buffers;
+static std::vector<gltf_buffer_view_t> gltf_buffer_views;
+static std::vector<gltf_accessor_t> gltf_accessors; 
 
 void gltf_skip_section() {
   // skip logic
-  
+  int num_opening_seen = 0;  
+  bool looking_for_comma = true;
+  char opening = 0;
+  char closing = 0;
+  while (true) {
+    if (looking_for_comma && gltf_peek() == ',') {
+      gltf_eat();
+      break;
+    } else if (!looking_for_comma && num_opening_seen == 0) {
+      break;
+    }
+    char peeked = gltf_peek();
+
+    if (peeked == '{' && opening == 0) {
+      opening = '{';
+      closing = '}';
+    } else if (peeked == '[' && opening == 0) {
+      opening = '[';
+      closing = ']';
+    }
+
+    if (peeked == opening) {
+      looking_for_comma = false;
+      num_opening_seen++;
+    } else if (peeked == closing) {
+      num_opening_seen--;
+      if (looking_for_comma) {
+        break;
+      }
+    }
+    gltf_eat();
+  }
+  if (!looking_for_comma && gltf_peek() == ',') {
+    gltf_eat();
+  }
 }
 
 void gltf_parse_asset_section() {
@@ -40,6 +79,7 @@ std::vector<int> gltf_parse_integer_array() {
       ints.push_back(val);
       continue;
     }
+    gltf_eat();
   }
   gltf_eat();
   data[offset-1] = 0;
@@ -55,6 +95,7 @@ void gltf_parse_scene() {
   inu_assert(gltf_peek() == '{');
   gltf_eat();
   while (gltf_peek() != '}') {
+#if 0
     inu_assert(gltf_peek() == '\"');
     gltf_eat();
     char* key_start = data + offset;
@@ -63,12 +104,18 @@ void gltf_parse_scene() {
     }
     gltf_eat();
     data[offset-1] = 0;
-    if (strcmp(key_start, "nodes") == 0) {
+#else
+    std::string key = gltf_parse_string();
+#endif
+
+    if (key == "nodes") {
       // get the root nodes
       inu_assert(gltf_peek() == ':');
       gltf_eat();
       scene.root_nodes = gltf_parse_integer_array();
       if (gltf_peek() == ',') gltf_eat();
+    } else {
+      gltf_skip_section();
     }
   }
   gltf_eat();
@@ -82,6 +129,7 @@ void gltf_parse_scenes_section() {
     gltf_parse_scene();
     if (gltf_peek() == ',') gltf_eat();
   }
+  gltf_eat();
 }
 
 void gltf_parse_node() {
@@ -91,6 +139,8 @@ void gltf_parse_node() {
   gltf_eat();
 
   while (gltf_peek() != '}') {
+
+#if 0
     inu_assert(gltf_peek() == '\"');
     gltf_eat();
     char* key_start = data + offset;
@@ -99,15 +149,19 @@ void gltf_parse_node() {
     }
     gltf_eat();
     data[offset-1] = 0;
-    if (strcmp(key_start, "children") == 0) {
+#else
+    std::string key = gltf_parse_string();
+#endif
+
+    inu_assert(gltf_peek() == ':');
+    gltf_eat();
+
+    if (key == "children") {
       // get the root nodes
-      inu_assert(gltf_peek() == ':');
-      gltf_eat();
-      node.child_node_handles = gltf_parse_integer_array();
+      node.child_node_idxs = gltf_parse_integer_array();
       if (gltf_peek() == ',') gltf_eat();
-    } else if (strcmp(key_start, "mesh") == 0) {
-      inu_assert(gltf_peek() == ':');
-      gltf_eat();
+    } else if (key == "mesh") {
+#if 0
       char* mesh_idx_str = data + offset;
       while (gltf_peek() != '}' && gltf_peek() != ',') {
         gltf_eat();
@@ -121,6 +175,12 @@ void gltf_parse_node() {
         node.gltf_mesh_handle = atoi(mesh_idx_str);
         data[offset] = '}';
       }
+#else
+      node.gltf_mesh_handle = gltf_parse_integer();
+      if (gltf_peek() == ',') gltf_eat();
+#endif
+    } else {
+      gltf_skip_section();
     }
   }
   gltf_eat();
@@ -134,9 +194,272 @@ void gltf_parse_nodes_section() {
     gltf_parse_node();
     if (gltf_peek() == ',') gltf_eat();
   }
+  gltf_eat();
+}
+
+std::string gltf_parse_string() {
+  inu_assert(gltf_peek() == '\"');
+  gltf_eat();
+  char* start = data + offset;
+  while (gltf_peek() != '\"') {
+    gltf_eat();
+  }
+  data[offset] = 0;
+  std::string val = start;
+  data[offset] = '\"';
+  gltf_eat();
+
+  return val;
+}
+
+gltf_attributes_t gltf_parse_attribs() {
+  gltf_attributes_t attrib;
+
+  inu_assert(gltf_peek() == '{');
+  gltf_eat();
+
+  while (gltf_peek() != '}') {
+    std::string key = gltf_parse_string();
+
+    inu_assert(gltf_peek() == ':');
+    gltf_eat();
+
+    if (key == "NORMAL") {
+      attrib.normals_accessor_idx = gltf_parse_integer();
+    } else if (key == "POSITION") {
+      attrib.positions_accessor_idx = gltf_parse_integer();
+    } else {
+      // not sure if this works for individual elements
+      gltf_skip_section();
+    }
+    if (gltf_peek() == ',') gltf_eat();
+  }  
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
+
+  return attrib;
+}
+
+gltf_primitive_t gltf_parse_primitive() {
+  gltf_primitive_t prim;
+
+  inu_assert(gltf_peek() == '{');
+  gltf_eat();
+
+  while (gltf_peek() != '}') {
+    std::string key = gltf_parse_string();
+
+    inu_assert(gltf_peek() == ':');
+    gltf_eat();
+
+    if (key == "attributes") {
+      prim.attribs = gltf_parse_attribs();
+    } else if (key == "indices") {
+      prim.indicies_accessor_idx = gltf_parse_integer();
+    } else if (key == "mode") {
+      prim.mode = gltf_parse_integer();
+    } else if (key == "material") {
+      prim.material_idx = gltf_parse_integer();
+    } else {
+      gltf_skip_section();
+    }
+  }
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
+
+  return prim;
+}
+
+std::vector<gltf_primitive_t> gltf_parse_primitives_section() {
+  std::vector<gltf_primitive_t> prims;
+  inu_assert(gltf_peek() == '[');
+  gltf_eat();
+  while (gltf_peek() != ']') {
+    gltf_primitive_t prim = gltf_parse_primitive();
+    prims.push_back(prim);
+  }
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
+  return prims;
+}
+
+void gltf_parse_mesh() {
+  gltf_mesh_t mesh;
+
+  inu_assert(gltf_peek() == '{');
+  gltf_eat();
+
+  while (gltf_peek() != '}') {
+#if 0
+    char* key_start = data + offset;
+    while (gltf_peek() != '\"') {
+      gltf_eat();
+    }
+    gltf_eat();
+    data[offset-1] = 0;
+#else
+    std::string key = gltf_parse_string();
+#endif
+
+    inu_assert(gltf_peek() == ':');
+    gltf_eat();
+
+    if (key == "primitives") {
+      mesh.primitives = gltf_parse_primitives_section();
+    } else if (key == "name") {
+      mesh.name = gltf_parse_string();
+    } else {
+      gltf_skip_section();
+    }
+
+    if (gltf_peek() == ',') gltf_eat();
+
+  }
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
+
+  gltf_meshes.push_back(mesh);
+}
+
+void gltf_parse_meshes_section() {
+  inu_assert(gltf_peek() == '[');
+  gltf_eat();
+  while (gltf_peek() != ']') {
+    gltf_parse_mesh();
+  }
+  gltf_eat();
+}
+
+int gltf_parse_integer() {
+  char* start = data + offset; 
+  int val = -1;
+  while (true) {
+    if (gltf_peek() == ',') {
+      gltf_eat();
+      data[offset-1] = 0;
+      val = atoi(start);
+      break;
+    } else if (gltf_peek() == '}') {
+      data[offset] = 0;
+      val = atoi(start);
+      data[offset] = '}';
+      break;
+    }
+    gltf_eat();
+  }
+  return val;
+}
+
+void gltf_parse_buffer() {
+  gltf_buffer_t buffer; 
+  inu_assert(gltf_peek() == '{');
+  gltf_eat();
+  while (gltf_peek() != '}') {
+    std::string key = gltf_parse_string();
+    inu_assert(gltf_peek() == ':');
+    gltf_eat();
+    if (key == "byteLength") {
+      buffer.byte_length = gltf_parse_integer();
+    } else if (key == "uri") {
+      buffer.uri = gltf_parse_string();
+    }
+    if (gltf_peek() == ',') gltf_eat();
+  }
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
+  gltf_buffers.push_back(buffer);
+}
+
+void gltf_parse_buffers_section() {
+  inu_assert(gltf_peek() == '[');
+  gltf_eat();
+  while (gltf_peek() != ']') {
+    gltf_parse_buffer();
+  }
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
+}
+
+void gltf_parse_accessor() {
+  gltf_accessor_t accessor; 
+  inu_assert(gltf_peek() == '{');
+  gltf_eat();
+  while (gltf_peek() != '}') {
+    std::string key = gltf_parse_string();
+    inu_assert(gltf_peek() == ':');
+    gltf_eat();
+    if (key == "bufferView") {
+      accessor.buffer_view_idx = gltf_parse_integer();
+    } else if (key == "byteOffset") {
+      accessor.byte_offset = gltf_parse_integer();
+    } else if (key == "componentType") {
+      accessor.component_type = gltf_parse_integer();
+    } else if (key == "count") {
+      accessor.count = gltf_parse_integer();
+    } else if (key == "type") {
+      accessor.type = gltf_parse_string();
+    } else {
+      gltf_skip_section();
+    }
+    if (gltf_peek() == ',') gltf_eat();
+  }
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
+  gltf_accessors.push_back(accessor);
+}
+
+void gltf_parse_accessors_section() {
+  inu_assert(gltf_peek() == '[');
+  gltf_eat();
+  while (gltf_peek() != ']') {
+    gltf_parse_accessor();
+  }
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
+}
+
+void gltf_parse_buffer_view() {
+  gltf_buffer_view_t buffer_view;
+
+  inu_assert(gltf_peek() == '{');
+  gltf_eat();
+
+  while (gltf_peek() != '}') {
+    std::string key = gltf_parse_string();
+    inu_assert(gltf_peek() == ':');
+    gltf_eat();
+    if (key == "buffer") {
+      buffer_view.gltf_buffer_index = gltf_parse_integer();
+    } else if (key == "byteOffset") {
+      buffer_view.byte_offset = gltf_parse_integer();
+    } else if (key == "byteLength") {
+      buffer_view.byte_length = gltf_parse_integer();
+    } else if (key == "byteStride") {
+      buffer_view.byte_stride = gltf_parse_integer();
+    } else if (key == "target") {
+      buffer_view.target = gltf_parse_integer();
+    }
+    if (gltf_peek() == ',') gltf_eat();
+  }
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
+
+  gltf_buffer_views.push_back(buffer_view);
+}
+
+void gltf_parse_buffer_views_section() {
+  inu_assert(gltf_peek() == '[');
+  gltf_eat();
+  while (gltf_peek() != ']') {
+    gltf_parse_buffer_view();
+  }
+  gltf_eat();
+  if (gltf_peek() == ',') gltf_eat();
 }
 
 void gltf_parse_section() {
+
+#if 0
   inu_assert(gltf_peek() == '\"');
   gltf_eat();
   char* key_start = data + offset;
@@ -145,16 +468,31 @@ void gltf_parse_section() {
   }
   gltf_eat();
   data[offset-1] = 0;
+#else
+  std::string key = gltf_parse_string();
+#endif
 
   inu_assert(gltf_peek() == ':', "colon is not seen");
   gltf_eat();
 
-  if (strcmp(key_start, "asset") == 0) {
+  if (key == "asset") {
     gltf_parse_asset_section();
-  } else if (strcmp(key_start, "nodes") == 0) {
+  } else if (key == "nodes") {
     gltf_parse_nodes_section();
-  } else if (strcmp(key_start, "scenes") == 0) {
+  } else if (key == "scenes") {
     gltf_parse_scenes_section();
+  } else if (key == "meshes") {
+    gltf_parse_meshes_section();
+  } else if (key == "scene") {
+    active_scene = gltf_parse_integer();
+  } else if (key == "buffers") {
+    gltf_parse_buffers_section();
+  } else if (key == "bufferViews") {
+    gltf_parse_buffer_views_section();
+  } else if (key == "accessors") {
+    gltf_parse_accessors_section();
+  } else {
+    gltf_skip_section();
   }
 
   if (gltf_peek() == ',') {
--- src/model_loading/gltf/gltf.h
+++ src/model_loading/gltf/gltf.h
@@ -3,13 +3,10 @@
 #include "model_loading/model_internal.h"
 
 #include <vector>
-
-struct gltf_mesh_t {
-
-};
+#include <string>
 
 struct gltf_node_t {
-  std::vector<int> child_node_handles;
+  std::vector<int> child_node_idxs;
   int gltf_mesh_handle = -1;
 };
 
@@ -17,10 +14,61 @@ struct gltf_scene_t {
   std::vector<int> root_nodes;
 };
 
+struct gltf_attributes_t {
+  int normals_accessor_idx = -1;
+  int positions_accessor_idx = -1;
+};
+
+struct gltf_primitive_t {
+  int indicies_accessor_idx = -1;
+  int material_idx = -1;
+  // gpu topology type
+  int mode = -1;
+  gltf_attributes_t attribs;
+};
+
+struct gltf_mesh_t {
+  std::string name;
+  std::vector<gltf_primitive_t> primitives; 
+};
+
+struct gltf_buffer_t {
+  int byte_length = -1;
+  std::string uri;
+};
+
+struct gltf_buffer_view_t {
+  int gltf_buffer_index = -1;
+  int byte_offset = -1;
+  int byte_length = -1;
+  int target = -1;
+  int byte_stride = 1; 
+};
+
+struct gltf_accessor_t {
+  int buffer_view_idx = -1;
+  int byte_offset = -1;
+  int component_type = -1;
+  int count = -1;
+  std::string type;
+};
+
+int gltf_parse_integer();
+std::string gltf_parse_string();
 std::vector<int> gltf_parse_integer_array();
+
+gltf_primitive_t gltf_parse_primitive();
+std::vector<gltf_primitive_t> gltf_parse_primitives_section();
+gltf_attributes_t gltf_parse_attribs();
+
+void gltf_skip_section();
 void gltf_parse_scenes_section();
 void gltf_parse_asset_section();
 void gltf_parse_nodes_section();
+void gltf_parse_meshes_section();
+void gltf_parse_buffers_section();
+void gltf_parse_buffer_views_section();
+void gltf_parse_accessors_section();
 
 void gltf_eat();
 void gltf_parse_section();
