595a637deed856c1d782e791dfb07080d4fa50fe
SarthakKamboj <sarthakkamboj22@gmail.com>
2024-02-24 17:26:32 -0600

    added comments in math.h

--- src/utils/math.h
+++ src/utils/math.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "vectors.h"
+
+// LIST OF FUNCTIONS YOU WILL NEED TO CREATE/IMPLEMENT
+// DO THESE FOR VECTOR2 AND VECTOR3s
+// dot product
+// cross product
+// vector multiplication
+//    - vector + vector
+//    - vector + float
+// vector magnitude
+// addition
+// subtraction
+// division (make sure not divide by 0)
+// normalize
