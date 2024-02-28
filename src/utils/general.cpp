0acd9e9f91b067606cc1fb2bd880eeb4c8343700
SarthakKamboj <sarthakkamboj22@gmail.com>
2024-02-28 14:05:49 -0600

    added helper function to get resources path

--- src/utils/general.cpp
+++ src/utils/general.cpp
@@ -3,8 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
 #include "log.h"
+#include "utils/app_info.h"
 
 char* get_file_contents(const char* full_path) {
   FILE* file = fopen(full_path, "r");
@@ -34,4 +36,26 @@ char* get_file_contents(const char* full_path) {
 
   fclose(file);
   return contents_buffer;
+}
+
+extern app_info_t app_info;
+void get_resources_folder_path(char path_buffer[256]) {	
+	static bool got_root_path = false;
+	static char s_folder[256]{};
+	if (!got_root_path) {
+		char folder[256]{};
+		if (app_info.running_in_vs) {
+		  GetCurrentDirectoryA(256, folder);
+		  char* slash_before_build_folder = strrchr(folder, '\\');
+		  *slash_before_build_folder = 0;
+      char* slash_before_out_folder = strrchr(folder, '\\');
+		  *slash_before_out_folder = 0;
+		} else {
+		  GetCurrentDirectoryA(256, folder);
+		}
+
+		sprintf(s_folder, "%s\\resources", folder);
+		got_root_path = true;
+	}
+	memcpy(path_buffer, s_folder, strlen(s_folder)); 
 }
--- src/utils/general.h
+++ src/utils/general.h
@@ -1,3 +1,4 @@
 #pragma once
 
 char* get_file_contents(const char* full_path);
+void get_resources_folder_path(char path_buffer[256]);
