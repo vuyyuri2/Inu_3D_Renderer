35d84d044224a570e4759ce7a132ca7869313982
SarthakKamboj <sarthakkamboj22@gmail.com>
2024-02-28 14:19:00 -0600

    changed file path to use get_resources_folder function output as relative pt

--- src/main.cpp
+++ src/main.cpp
@@ -16,25 +16,30 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     app_info.running_in_vs = true;
   }
 
-  create_window(hInstance, 400, 300); 
-
-  // shader_t shader = create_shader("C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\shaders\\model.vert", "C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\shaders\\model.frag");
-  material_t::associated_shader = create_shader("C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\shaders\\model.vert", "C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\shaders\\model.frag");
-
   char resources_path[256]{};
   get_resources_folder_path(resources_path);
   printf("resources_path: %s\n", resources_path);
 
-  std::vector<model_t> models;
-  // gltf_load_file("C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\box\\Box.gltf", models);
-  // gltf_load_file("C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\box_interleaved\\BoxInterleaved.gltf", models);
-  // gltf_load_file("C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\box_textured\\BoxTextured.gltf", models);
-  // gltf_load_file("C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\box_textured_non_power_of_2\\BoxTexturedNonPowerOfTwo.gltf", models);
-  // gltf_load_file("C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\box_with_spaces\\Box With Spaces.gltf", models);
-  gltf_load_file("C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\box_vertex_colors\\BoxVertexColors.gltf", models);
+  create_window(hInstance, 400, 300); 
 
-  // gltf_load_file("C:\\Sarthak\\projects\\3d_anim_renderer\\resources\\duck\\Duck.gltf", models);
-  // float angle = 0;
+  char vert_shader_path[256]{};
+  sprintf(vert_shader_path, "%s\\shaders\\model.vert", resources_path);
+  char frag_shader_path[256]{};
+  sprintf(frag_shader_path, "%s\\shaders\\model.frag", resources_path);
+  material_t::associated_shader = create_shader(vert_shader_path, frag_shader_path); 
+
+  // const char* gltf_file_resources_folder_rel_path =  "box\\Box.gltf";
+  // const char* gltf_file_resources_folder_rel_path =  "box_interleaved\\BoxInterleaved.gltf";
+  // const char* gltf_file_resources_folder_rel_path = "box_textured\\BoxTextured.gltf";
+  // const char* gltf_file_resources_folder_rel_path = "box_textured_non_power_of_2\\BoxTexturedNonPowerOfTwo.gltf";
+  // const char* gltf_file_resources_folder_rel_path = "box_with_spaces\\Box With Spaces.gltf";
+  const char* gltf_file_resources_folder_rel_path = "box_vertex_colors\\BoxVertexColors.gltf";
+
+  std::vector<model_t> models;
+  char gltf_full_file_path[256]{};
+  sprintf(gltf_full_file_path, "%s\\%s", resources_path, gltf_file_resources_folder_rel_path);
+  gltf_load_file(gltf_full_file_path, models);
+
   while (window.running) {
     poll_events();
 
