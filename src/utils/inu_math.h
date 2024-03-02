d0ba642b9cf25920e0659f537ea0a548926eae7b
SarthakKamboj <sarthakkamboj22@gmail.com>
2024-03-02 17:22:25 -0600

    added to vectors.h and made comments to inu_math.h on naming convention stuff and organization things

--- src/utils/inu_math.h
+++ src/utils/inu_math.h
@@ -2,6 +2,36 @@
 
 #include "vectors.h"
 
+// instead of creating the vector2d class here...use the vec2/ivec2/vec3 structs from vectors.h
+// for right now...don't do anything for vec4 from that file
+
+// The idea behind this is that vectors.h just contains the vector data structures
+// but inu_math.h will contain the actual math functionality with these data structures
+// b/c they are more so math helper functions
+
+/*
+	EXAMPLE
+
+vec2 addition(const vec2& vect1, const vec2& vect2);
+vec2 subtraction(const vec2& vect1, const vec2& vect2);
+vec2 multiplication(const vec2& vect, float scalar);
+vec2 multiplication(const vec2& vect1, const vec2& vect1);
+etc...
+
+ivec2 addition(const ivec2& vect1, const ivec2& vect2);
+ivec2 subtraction(const ivec2& vect1, const ivec2& vect2);
+ivec2 multiplication(const ivec2& vect, float scalar);
+ivec2 multiplication(const ivec2& vect1, const ivec2& vect1);
+etc...
+
+vec3 addition(const vec3& vect1, const vec3& vect2);
+vec3 subtraction(const vec3& vect1, const vec3& vect2);
+vec3 multiplication(const vec3& vect, float scalar);
+vec3 multiplication(const vec3& vect1, const vec3& vect1);
+etc...
+
+ */
+
 class Vector2D {
 public: 
 	float x;
@@ -26,8 +56,6 @@ public:
 	Vector2D normalize() const;
 	float dotproduct(const Vector2D& vect) const;
 	float crossproduct(const Vector2D& vect) const;
-
-
 };
 
 class Vector3D {
--- src/utils/vectors.h
+++ src/utils/vectors.h
@@ -9,3 +9,16 @@ struct ivec2 {
   int x = 0;
   int y = 0;
 };
+
+struct vec3 {
+  float x = 0;
+  float y = 0;
+  float z = 0;
+};
+
+struct vec4 {
+  float x = 0;
+  float y = 0;
+  float z = 0;
+  float w = 0;
+};
