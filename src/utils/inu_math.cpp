bf9fb1706a712e3dfdb60a8935cc250f0aed2c9f
vuyyuri2 <vuyyuri2>
2024-04-01 13:45:26 -0500

    created test

--- src/utils/inu_math.cpp
+++ src/utils/inu_math.cpp
@@ -1,1 +1,70 @@
+#include <cmath> 
+
 #include "inu_math.h"
+
+float dot_product(const vec2& vect1, const vec2& vect2) {
+    return vect1.x * vect2.x + vect1.y * vect2.y;
+}
+
+float dot_product(const vec3& vect1, const vec3& vect2) {
+    return vect1.x * vect2.x + vect1.y * vect2.y + vect1.z * vect2.z;
+}
+
+vec3 cross_product(const vec3& vect1, const vec3& vect2) {
+    return {
+        vect1.y * vect2.z - vect1.z * vect2.y,
+        vect1.z * vect2.x - vect1.x * vect2.z,
+        vect1.x * vect2.y - vect1.y * vect2.x
+    };
+}
+
+float magnitude(const vec2& vect) {
+    return std::sqrt(vect.x * vect.x + vect.y * vect.y);
+}
+
+float magnitude(const vec3& vect) {
+    return std::sqrt(vect.x * vect.x + vect.y * vect.y + vect.z * vect.z);
+}
+
+vec2 addition(const vec2& vect1, const vec2& vect2) {
+    return { vect1.x + vect2.x, vect1.y + vect2.y };
+}
+
+vec2 subtraction(const vec2& vect1, const vec2& vect2) {
+    return { vect1.x - vect2.x, vect1.y - vect2.y };
+}
+
+vec2 division(const vec2& vect, float scalar) {
+    if (scalar != 0)
+        return { vect.x / scalar, vect.y / scalar };
+    else
+        return { 0, 0 }; 
+
+vec3 addition(const vec3& vect1, const vec3& vect2) {
+    return { vect1.x + vect2.x, vect1.y + vect2.y, vect1.z + vect2.z };
+}
+
+vec3 subtraction(const vec3& vect1, const vec3& vect2) {
+    return { vect1.x - vect2.x, vect1.y - vect2.y, vect1.z - vect2.z };
+}
+
+vec3 division(const vec3& vect, float scalar) {
+    if (scalar != 0)
+        return { vect.x / scalar, vect.y / scalar, vect.z / scalar };
+    else
+        return { 0, 0, 0 }; 
+}
+
+vec2 normalize(const vec2& vect) {
+    float mag = magnitude(vect);
+    if (mag != 0)
+        return { vect.x / mag, vect.y / mag };
+    else
+        return { 0, 0 }; 
+
+vec3 normalize(const vec3& vect) {
+    float mag = magnitude(vect);
+    if (mag != 0)
+        return { vect.x / mag, vect.y / mag, vect.z / mag };
+    else
+        return { 0, 0, 0 }; 
--- src/utils/inu_math.h
+++ src/utils/inu_math.h
@@ -14,3 +14,26 @@
 // subtraction
 // division (make sure not divide by 0)
 // normalize
+
+
+struct vec2;
+struct vec3;
+
+float dot_product(const vec2& vect1, const vec2& vect2);
+float dot_product(const vec3& vect1, const vec3& vect2);
+
+vec3 cross_product(const vec3& vect1, const vec3& vect2);
+
+float magnitude(const vec2& vect);
+float magnitude(const vec3& vect);
+
+vec2 addition(const vec2& vect1, const vec2& vect2);
+vec2 subtraction(const vec2& vect1, const vec2& vect2);
+vec2 division(const vec2& vect, float scalar);
+
+vec3 addition(const vec3& vect1, const vec3& vect2);
+vec3 subtraction(const vec3& vect1, const vec3& vect2);
+vec3 division(const vec3& vect, float scalar);
+
+vec2 normalize(const vec2& vect);
+vec3 normalize(const vec3& vect);
